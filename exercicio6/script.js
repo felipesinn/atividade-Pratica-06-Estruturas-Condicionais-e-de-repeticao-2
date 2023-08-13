43c4f1c550c961d7eecfb79b5116dc6f6c2984cf
felipesinn <felipesinn01@gmail.com>
2023-08-13 09:38:52 -0300

    exer 6 ok

---
+++ exercicio6/script.js
@@ -0,0 +1,14 @@
+let quantidadeNumeros = 0;
+let valorAtual = 1;
+
+while (quantidadeNumeros < 30) {
+    if (valorAtual % 2 !== 0) {
+        console.log(valorAtual);
+        quantidadeNumeros++;
+    }
+    valorAtual++;
+}
+
+
+
+// 6. Insira no html 30 ( trinta ) números ímpares.
